1e17518c8a8b926c10da0a43bdb6c824259e4652
Kaio Delphino <kaio@tutamail.com>
2022-01-07 19:51:02 -0300

    feature: implemented utils folder

---
+++ lib/utils/constants.dart
@@ -0,0 +1,2 @@
+const host = "10.5.40.40";
+const port = "5000";
---
+++ lib/utils/enums.dart
@@ -0,0 +1,5 @@
+
+// rest api response status
+enum ResponseStatus { success, err }
+// rest api endpoint
+enum Endpoint { tickets, appointments, agenda }
