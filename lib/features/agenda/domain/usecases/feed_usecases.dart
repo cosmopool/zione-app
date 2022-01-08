786a20d239874334ab39595c9e013c90b6e4077e
Kaio Delphino <kaio@tutamail.com>
2022-01-08 01:28:58 -0300

    refactor: simplified method parameter

--- lib/features/agenda/domain/usecases/feed_usecases.dart
+++ lib/features/agenda/domain/usecases/feed_usecases.dart
@@ -1,9 +1,8 @@
 import 'package:zione/features/agenda/domain/entities/entry_model.dart';
-import 'package:zione/features/agenda/domain/entities/entry_card_model.dart';
 
 abstract class FeedUseCases {
   void insertCard(Entry entry);
-  void deleteCard(EntryCard entryCard);
+  void deleteCard(int cardId);
   void expandCard(int cardId);
   void closeCard();
   void refresh();
