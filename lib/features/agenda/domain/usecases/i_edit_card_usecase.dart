e187da9c94bf8c1176ce64a1ec93602a2a37b3b2
Kaio Delphino <kaio@tutamail.com>
2022-01-10 21:20:45 -0300

    Revert "refactor: implemented single responsability usecase classes"
    This reverts commit c2cc658a58c1686ca3ae8df110e1406e55e94e8e.

--- lib/features/agenda/domain/usecases/i_edit_card_usecase.dart
+++
@@ -1,8 +0,0 @@
-import 'package:zione/features/agenda/domain/entities/entry_entity.dart';
-import 'package:zione/utils/enums.dart';
-
-abstract class IEditCardUsecase {
-  Future<bool> call(EntryEntity entry, Endpoint endpoint);
-}
-
-class EditCardUsecase extends EditCardUsecase {}
--- lib/features/agenda/domain/usecases/i_refresh_feed_usecase.dart
+++
@@ -1,7 +0,0 @@
-import 'package:zione/utils/enums.dart';
-
-abstract class IRefreshFeedUsecase {
-  Future<bool> call(Endpoint endpoint);
-}
-
-class RefreshFeedUsecase extends IRefreshFeedUsecase {}
