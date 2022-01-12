11c8eb5f6bb155989fd45e08d1978c0b06ab25d2
Kaio Delphino <kaio@tutamail.com>
2022-01-12 16:01:50 -0300

    feature: implemented authentication token and singleton class use

--- test/domain/usecases/refresh_feed_usecase_test.dart
+++ test/domain/usecases/refresh_feed_usecase_test.dart
@@ -1,26 +1,24 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:zione/features/agenda/data/datasources/refresh_feed_datasource.dart';
-import 'package:zione/features/agenda/data/datasources/rest_api_server/rest_api_datasource.dart';
-import 'package:zione/features/agenda/domain/repositories/i_refresh_feed_repository.dart';
+import 'package:get_it/get_it.dart';
+import 'package:zione/core/auth.dart';
+import 'package:zione/core/dependency_injection.dart';
 import 'package:zione/features/agenda/domain/usecases/i_refresh_feed_usecase.dart';
-import 'package:zione/features/agenda/infra/datasources/i_refresh_feed_datasource.dart';
 import 'package:zione/features/agenda/infra/datasources/i_response_api_request.dart';
-import 'package:zione/features/agenda/infra/repositories/refresh_feed_repository.dart';
 import 'package:zione/utils/enums.dart';
 
+import '../../authorizarion_token_for_tests.dart';
 import 'agenda_endpoint_json_response.dart';
 
 main() {
-  // test('Should return error when not able to connect with remote server', () {
-  // });
+  token = testToken;
+  Inject.init();
 
-  test('Should return list of appointments when refresh agenda endpoint', () async {
-    final server = ApiServerDataSource();
-    IRefreshFeedDataSouce datasource = RefreshFeedDataSource(server);
-    IRefreshFeedRepository repository = RefreshFeedRepository(datasource);
-    IRefreshFeedUsecase refresh = RefreshFeedUsecase(repository);
+  test('Should return list of appointments when refresh agenda endpoint',
+      () async {
+    final IRefreshFeedUsecase _refresh =
+        GetIt.instance.get<IRefreshFeedUsecase>();
 
-    IResponse response = await refresh(Endpoint.agenda);
+    IResponse response = await _refresh(Endpoint.agenda);
     const Map<String, dynamic> responseExpected = responseMap;
 
     expect(response.result, responseExpected['Result']);
