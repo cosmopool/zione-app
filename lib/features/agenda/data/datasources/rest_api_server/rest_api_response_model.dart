e3566ae3af5d61cb2cdb1126373112ab7e7bf169
Kaio Delphino <kaio@tutamail.com>
2022-01-25 10:48:31 -0300

    feature: implemented database response adapters

--- lib/features/agenda/data/datasources/rest_api_server/rest_api_response_model.dart
+++ lib/features/agenda/data/datasources/rest_api_server/rest_api_response_model.dart
@@ -1,35 +1,41 @@
-import 'dart:convert';
-
+import 'package:hive/hive.dart';
 import 'package:zione/features/agenda/infra/datasources/i_response_api_request.dart';
 import 'package:zione/utils/enums.dart';
 
+part 'rest_api_response_model.g.dart';
+
+@HiveType(typeId: 1)
 class Response implements IResponse {
-  @override
-  late ResponseStatus status;
-  @override
-  late var result;
+  @HiveField(0)
+  late ResponseStatus _status;
+  @HiveField(1)
+  late dynamic _result;
+  @HiveField(2)
+  bool _offline = false;
 
   @override
+  ResponseStatus get status => _status;
+  @override
+  get result => _result;
+  @override
+  bool get isSuccessful => (status == ResponseStatus.success);
+  // @HiveField(3)
+  @override
+  bool get isServerOffline => _offline;
+
   Response(Map json) {
     switch (json['Status']) {
-      case 'Success': { status = ResponseStatus.success; } break;
-      case 'Error': { status = ResponseStatus.err; } break;
-      default: { status = ResponseStatus.err; } break;
+      case 'Success': _status = ResponseStatus.success; break;
+      case 'Error': _status = ResponseStatus.err; break;
+      default: _status = ResponseStatus.err; break;
     }
 
-    if (json['Result'] != null) result = json['Result'];
+    if (json['Result'] != null) _result = json['Result'];
   }
 
   Response.noConnection(){
-    status = ResponseStatus.err;
-    result = "No connection with remote server";
-  }
-
-  List resultToList() {
-    if (status == ResponseStatus.success) {
-      return jsonDecode(result);
-    } else {
-      return [];
-    }
+    _status = ResponseStatus.err;
+    _result = "No connection with remote server";
+    _offline = true;
   }
 }
