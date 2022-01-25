b8e3bb74c274aedca750bb8ed39aafc0cf9627a6
Kaio Delphino <kaio@tutamail.com>
2022-01-25 10:57:43 -0300

    refactor: change properties access to getters

--- lib/features/agenda/infra/datasources/i_response_api_request.dart
+++ lib/features/agenda/infra/datasources/i_response_api_request.dart
@@ -1,6 +1,8 @@
 import 'package:zione/utils/enums.dart';
 
 abstract class IResponse {
-  late ResponseStatus status;
-  late var result;
+  ResponseStatus get status;
+  get result;
+  bool get isSuccessful;
+  bool get isServerOffline;
 }
